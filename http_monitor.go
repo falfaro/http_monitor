337e91808f53a72b4b0f8c8fe128031b97dbb998
Felipe Alfaro Solana <felipe.alfaro@teralytics.ch>
2019-09-18 13:25:07 +0200

    Initial support for tail and dumping stats to standard output.

--- http_monitor.go
+++ http_monitor.go
@@ -2,11 +2,20 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"regexp"
+	"sort"
 	"strconv"
+	"sync"
 	"time"
+
+	"github.com/hpcloud/tail"
 )
 
+// Timestamp format used in W3C-formatted access logs
+const strftime = "_2/Jan/2006:15:04:05 -0700"
+
+// Log record
 type Log struct {
 	IP         string
 	Identity   string
@@ -20,11 +29,17 @@ type Log struct {
 	Size       int
 }
 
-const strftime = "02/Jan/2006:15:04:05 +0000"
+type stats struct {
+	// Key is the HTTP Response/Status Code, typically collapsed into
+	// `1XX`, `2XX`, `3XX`, etc. to produce a less, more compact and
+	// meaningful map.
+	httpResponseCodes map[string]int
+	sectionCounts     map[string]int
+}
 
-var logLineRegExp = regexp.MustCompile(`([^ ]+) (-) ([[:alnum:]]+) ` +
+var logLineRegExp = regexp.MustCompile(`([^ ]+) (-) ([0-9A-Za-z-]+) ` +
 	// Timestamp
-	`\[(\d{2}/(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/\d{4}:\d{2}:\d{2}:\d{2} \+\d{4})\]` +
+	`\[(\d{2}/(Jan|Feb|Mar|Apr|May|Jun|Jul|Aug|Sep|Oct|Nov|Dec)/\d{4}:\d{2}:\d{2}:\d{2} [+-]\d{4})\]` +
 	// Methpd
 	` \"(GET|POST|PUT|HEAD|DELETE|OPTIONS) ` +
 	// Section
@@ -36,7 +51,7 @@ var logLineRegExp = regexp.MustCompile(`([^ ]+) (-) ([[:alnum:]]+) ` +
 	// Status code
 	`(\d{3}) ` +
 	// Size
-	`(\d+)`)
+	`([0-9-]+)`)
 
 func parseLogLine(s string) (*Log, error) {
 	var ts time.Time
@@ -45,8 +60,11 @@ func parseLogLine(s string) (*Log, error) {
 	var size int
 
 	matched := logLineRegExp.FindStringSubmatch(s)
+	if len(matched) < 11 {
+		log.Panicf("Error parsing log line: %s", s)
+	}
 
-	if ts, err = time.Parse(strftime, matched[4]); err != nil {
+	if ts, err = time.ParseInLocation(strftime, matched[4], time.UTC); err != nil {
 		return nil, err
 	}
 
@@ -55,7 +73,7 @@ func parseLogLine(s string) (*Log, error) {
 	}
 
 	if size, err = strconv.Atoi(matched[11]); err != nil {
-		return nil, err
+		size = 0
 	}
 
 	return &Log{
@@ -72,9 +90,98 @@ func parseLogLine(s string) (*Log, error) {
 	}, nil
 }
 
+// Update stats
+func updateStats(s *stats, log *Log) {
+	// Generate a 1XX, 2XX, 3XX, 4XX or 5XX string from the response code
+	responseCode := fmt.Sprintf("%d", log.StatusCode)
+	responseCode = fmt.Sprintf("%cXX", responseCode[0])
+	s.httpResponseCodes[responseCode]++
+	s.sectionCounts[log.Section]++
+}
+
+// Dump stats to standard output
+func dumpStats(s *stats) {
+	fmt.Println("---")
+	dumpResponseCodes(s)
+	dumpTopSections(s, 5)
+}
+
+// Dump HTTP response codes to standard output
+func dumpResponseCodes(s *stats) {
+	fmt.Printf("Response codes:\n")
+
+	var keys []string
+	for k := range s.httpResponseCodes {
+		keys = append(keys, k)
+	}
+	sort.Strings(keys)
+
+	for _, k := range keys {
+		fmt.Printf("%10d (%s)\n", s.httpResponseCodes[k], k)
+	}
+}
+
+// Dumps the top N sections to standard output
+func dumpTopSections(s *stats, n int) {
+	type sectionCountPair struct {
+		count   int
+		section string
+	}
+
+	counts := make([]sectionCountPair, 0, len(s.sectionCounts))
+	for section, count := range s.sectionCounts {
+		counts = append(counts, sectionCountPair{count: count, section: section})
+	}
+	sort.Slice(counts, func(i, j int) bool {
+		return counts[i].count > counts[j].count
+	})
+	fmt.Printf("Top %d sections:\n", n)
+	for i, v := range counts {
+		if i >= n {
+			break
+		}
+		fmt.Printf("%10d (%s)\n", v.count, v.section)
+	}
+}
+
 func main() {
-	s := "127.0.0.1 - jill [09/May/2018:16:00:41 +0000] \"GET /api/user HTTP/1.0\" 200 234"
-	if log, err := parseLogLine(s); err == nil {
-		fmt.Printf("%+v\n", log)
+	s := &stats{
+		sectionCounts: make(map[string]int),
+		httpResponseCodes: map[string]int{
+			"1XX": 0,
+			"2XX": 0,
+			"3XX": 0,
+			"4XX": 0,
+			"5XX": 0,
+		},
+	}
+
+	var mutex = &sync.Mutex{}
+
+	t, err := tail.TailFile("access.log", tail.Config{Follow: true})
+	if err != nil {
+		panic(err)
+	}
+
+	go func() {
+		for {
+			mutex.Lock()
+			dumpStats(s)
+			mutex.Unlock()
+			time.Sleep(10 * time.Second)
+		}
+	}()
+
+	// Tail through the access log file
+	for line := range t.Lines {
+		// Make processing intentionally slower
+		time.Sleep(500 * time.Microsecond)
+		parsedLog, err := parseLogLine(line.Text)
+		if err != nil {
+			panic(err)
+		}
+		mutex.Lock()
+		updateStats(s, parsedLog)
+		mutex.Unlock()
 	}
 }
